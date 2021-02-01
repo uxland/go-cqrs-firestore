6100094f7b76475f6f66697e3835b9a6e50ba73d
dpinart <dpinart@uxland.cat>
2021-02-01 16:08:41 +0100

    feat: Query by filter feature added

--- read-db.go
+++ read-db.go
@@ -6,12 +6,19 @@ import (
 	"google.golang.org/api/iterator"
 )
 
+type Filter struct {
+	Path  string
+	Op    string
+	Value interface{}
+}
+
 type GenericReadDB interface {
 	SaveItem(transaction *firestore.Transaction, id string, item interface{}) error
 	LoadAllItems() (interface{}, error)
 	LoadItem(id string) (interface{}, error)
 	DeleteItem(transaction *firestore.Transaction, id string) error
 	UpdateItem(transaction *firestore.Transaction, id string, updates []firestore.Update) error
+	ListItems(filter []Filter, limit int) (interface{}, error)
 }
 type genericDBImpl struct {
 	collectionName string
@@ -22,6 +29,21 @@ type genericDBImpl struct {
 func NewGenericDBImpl(collectionName string, itemFactory func() interface{}, client *firestore.Client) GenericReadDB {
 	return &genericDBImpl{collectionName: collectionName, itemFactory: itemFactory, client: client}
 }
+
+func (g *genericDBImpl) ListItems(filter []Filter, limit int) (interface{}, error) {
+	col := g.client.Collection(g.collectionName)
+	if limit == 0 {
+		limit = 100
+	}
+	q := col.Query
+	for _, f := range filter {
+		q = q.Where(f.Path, f.Op, f.Value)
+	}
+	q = q.Limit(limit)
+	documentIterator := q.Documents(context.Background())
+	return g.readIterator(documentIterator)
+}
+
 func (g *genericDBImpl) DeleteItem(transaction *firestore.Transaction, id string) error {
 	collection := g.client.Collection(g.collectionName)
 	docRef := collection.Doc(id)
@@ -40,9 +62,7 @@ func (g *genericDBImpl) UpdateItem(transaction *firestore.Transaction, id string
 	return transaction.Update(docRef, updates)
 }
 
-func (g *genericDBImpl) LoadAllItems() (interface{}, error) {
-	collection := g.client.Collection(g.collectionName)
-	documentIterator := collection.Limit(1000).Documents(context.Background())
+func (g *genericDBImpl) readIterator(documentIterator *firestore.DocumentIterator) (interface{}, error) {
 	docs := make([]interface{}, 0)
 	for {
 		doc, err := documentIterator.Next()
@@ -60,6 +80,12 @@ func (g *genericDBImpl) LoadAllItems() (interface{}, error) {
 		docs = append(docs, item)
 	}
 	return docs, nil
+}
+
+func (g *genericDBImpl) LoadAllItems() (interface{}, error) {
+	collection := g.client.Collection(g.collectionName)
+	documentIterator := collection.Limit(1000).Documents(context.Background())
+	return g.readIterator(documentIterator)
 }
 
 func (g *genericDBImpl) LoadItem(id string) (interface{}, error) {
